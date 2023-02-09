f2123aaec6371d5c9860623787aa95ca2fdee72e
paulorochacode <paulorochacode@gmail.com>
2023-02-09 19:49:49 -0300

    Restricoes completo

--- src/test/java/Restricoes/ObrigatoriedadeTest.java
+++ src/test/java/Restricoes/ObrigatoriedadeTest.java
@@ -9,16 +9,7 @@ import static org.hamcrest.core.StringContains.containsString;
 
 public class ObrigatoriedadeTest {
     static class vars{
-        static String[] cpfRestrito ={  "97093236014",
-                "60094146012",
-                "84809766080",
-                "62648716050",
-                "26276298085",
-                "01317496094",
-                "55856777050",
-                "19626829001",
-                "24094592008",
-                "58063164083"};
+        static String[] cpf ={""};
     }
     class EndPoints{
         final String CONSULTAR_RESTRICOES_ENDPOINT = "/v1/restricoes/";
@@ -34,7 +25,7 @@ public class ObrigatoriedadeTest {
     @Test
     public void testConsultarRestricaoCPF0DeveRetornarStatusCode404() {
         given()
-                .get("v1/restricoes/"+ vars.cpfRestrito[0] )
+                .get("v1/restricoes/"+ vars.cpf[0] )
                 .then()
                 .statusCode(404)
                 .log().all()
