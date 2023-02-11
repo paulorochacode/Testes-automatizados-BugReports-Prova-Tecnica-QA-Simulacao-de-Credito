6350357ddb81dc219a38ea3b15964ca51e77664d
paulorochacode <paulorochacode@gmail.com>
2023-02-11 16:34:38 -0300

    Read

--- src/test/java/Simulacoes/Read/ReadAllTest.java
+++ src/test/java/Simulacoes/Read/ReadAllTest.java
@@ -1,5 +1,82 @@
 package Simulacoes.Read;
 
+import Restricoes.CPFsComRestricoesTest;
+import Simulacoes.Create.CreateObrigatoriedadeTest;
+import Simulacoes.Create.CreateTest;
+import br.com.sicredi.simulacao.entity.Simulacao;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 public class ReadAllTest {
+
+    @BeforeClass
+    public static void setup() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        baseURI = "http://localhost";
+        port = 8080;
+        basePath = "/api/";
+    }
+
+    @Test
+    public void test1CriandoSimulacaoDeveRetornarStatusCode201() {
+        given()
+            .body("{\n" +
+                    "\"nome\": \"Colaborador Sicredi\",\n" +
+                    "\"email\": \"fulano@gmail.com\",\n" +
+                    "\"cpf\": \"47290390407\",\n" +
+                    "\"valor\": 23040,\n" +
+                    "\"parcelas\": 6,\n" +
+                    "\"seguro\": false\n" +
+                    "}")
+            .contentType(ContentType.JSON)
+        .when()
+            .post("v1/simulacoes")
+        .then();
+        //    .log().all()
+        //    .statusCode(201);
+    }
+
+    @Test
+    public void test2CriandoSimulacaoDeveRetornarStatusCode201() {
+        given()
+            .body("{\n" +
+                    "\"nome\": \"Colaborador Sicredi\",\n" +
+                    "\"email\": \"fulano@gmail.com\",\n" +
+                    "\"cpf\": \"47290391207\",\n" +
+                    "\"valor\": 23640,\n" +
+                    "\"parcelas\": 7,\n" +
+                    "\"seguro\": false\n" +
+                    "}")
+            .contentType(ContentType.JSON)
+        .when()
+            .post("v1/simulacoes")
+        .then();
+        //    .log().all()
+        //    .statusCode(201);
+    }
+
+    @Test
+    public void test3ConsultandoTodasSimulacoesDeveRetornarStatusCode200() {
+        given()
+            .get("v1/simulacoes")
+        .then()
+            .statusCode(200)
+            .body(containsString("47290390407"));
+    }
+
+    @Test
+    public void test4ConsultandoTodasSimulacoesDeveRetornarStatusCode200() {
+        given()
+                .get("v1/simulacoes")
+                .then()
+                .statusCode(200)
+                .body(containsString("47290391207"));
+    }
 }
--- src/test/java/Simulacoes/Read/ReadOneTest.java
+++ src/test/java/Simulacoes/Read/ReadOneTest.java
@@ -1,4 +1,48 @@
 package Simulacoes.Read;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.basePath;
+import static org.hamcrest.core.StringContains.containsString;
+
 public class ReadOneTest {
+
+    @BeforeClass
+    public static void setup() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        baseURI = "http://localhost";
+        port = 8080;
+        basePath = "/api/";
+    }
+    @Test
+    public void test1CriandoSimulacaoDeveRetornarStatusCode201() {
+        given()
+                .body("{\n" +
+                        "\"nome\": \"Colaborador Sicredi\",\n" +
+                        "\"email\": \"fulano@gmail.com\",\n" +
+                        "\"cpf\": \"46490391207\",\n" +
+                        "\"valor\": 23640,\n" +
+                        "\"parcelas\": 7,\n" +
+                        "\"seguro\": false\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("v1/simulacoes")
+                .then();
+        //    .log().all()
+        //    .statusCode(201);
+    }
+
+    @Test
+    public void test2ConsultandoUmaSimulacoesDeveRetornarStatusCode200() {
+        given()
+                .get("v1/simulacoes")
+                .then()
+                .statusCode(200)
+                .body(containsString("46490390407"));
+    }
 }
